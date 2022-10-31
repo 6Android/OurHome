7234d1921c4f305a81f426fda50cf04ba7c7aec0
super7615 <super7615@naver.com>
2022-10-31 16:19:54 +0900

    feat : 테마 정의 (S07P31D203-156)

--- OurHome/presentation/src/main/java/com/ssafy/ourhome/ui/theme/Theme.kt
+++ OurHome/presentation/src/main/java/com/ssafy/ourhome/ui/theme/Theme.kt
@@ -1,19 +1,12 @@
 package com.ssafy.ourhome.ui.theme
 
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
+import androidx.compose.material.MaterialTheme.colors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
-private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
-)
-
 private val LightColorPalette = lightColors(
-    primary = Purple500,
+    primary = MainColor,
     primaryVariant = Purple700,
     secondary = Teal200
 
@@ -28,15 +21,9 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun OurHomeTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
-
+fun OurHomeTheme(content: @Composable () -> Unit) {
     MaterialTheme(
-        colors = colors,
+        colors = LightColorPalette,
         typography = Typography,
         shapes = Shapes,
         content = content
--- OurHome/presentation/src/main/java/com/ssafy/ourhome/ui/theme/Type.kt
+++ OurHome/presentation/src/main/java/com/ssafy/ourhome/ui/theme/Type.kt
@@ -1,28 +1,103 @@
 package com.ssafy.ourhome.ui.theme
 
 import androidx.compose.material.Typography
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.ssafy.ourhome.R
+
+private val hannar = FontFamily(
+    Font(R.font.hannar, FontWeight.Normal),
+)
+
+private val nanum = FontFamily(
+    Font(R.font.nanumsquareround_l, FontWeight.Light),
+    Font(R.font.nanumsquareround_r, FontWeight.Normal),
+    Font(R.font.nanumsquareround_b, FontWeight.Bold),
+    Font(R.font.nanumsquareround_eb, FontWeight.ExtraBold)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    body1 = TextStyle(
-        fontFamily = FontFamily.Default,
+    h1 = TextStyle(
+        fontFamily = hannar,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    )
-    /* Other default text styles to override
+        fontSize = 96.sp,
+        letterSpacing = (-1.5).sp
+    ),
+    h2 = TextStyle(
+        fontFamily = hannar,
+        fontWeight = FontWeight.Normal,
+        fontSize = 60.sp,
+        letterSpacing = (-0.5).sp
+    ),
+    h3 = TextStyle(
+        fontFamily = hannar,
+        fontWeight = FontWeight.Normal,
+        fontSize = 48.sp,
+        letterSpacing = 0.sp
+    ),
+    h4 = TextStyle(
+        fontFamily = hannar,
+        fontWeight = FontWeight.Normal,
+        fontSize = 34.sp,
+        letterSpacing = 0.25.sp
+    ),
+    h5 = TextStyle(
+        fontFamily = hannar,
+        fontWeight = FontWeight.Normal,
+        fontSize = 24.sp,
+        letterSpacing = 0.sp
+    ),
+    h6 = TextStyle(
+        fontFamily = hannar,
+        fontWeight = FontWeight.Normal,
+        fontSize = 20.sp,
+        letterSpacing = 0.15.sp
+    ),
+    subtitle1 = TextStyle(
+        fontFamily = hannar,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        letterSpacing = 0.15.sp
+    ),
+    subtitle2 = TextStyle(
+        fontFamily = hannar,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        letterSpacing = 0.1.sp
+    ),
+    body1 = TextStyle(
+        fontFamily = nanum,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        letterSpacing = 0.5.sp
+    ),
+    body2 = TextStyle(
+        fontFamily = nanum,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        letterSpacing = 0.25.sp
+    ),
     button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
+        fontFamily = nanum,
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp,
+        letterSpacing = 1.25.sp
     ),
     caption = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = nanum,
+        fontWeight = FontWeight.Bold,
+        fontSize = 12.sp,
+        letterSpacing = 0.4.sp
+    ),
+    overline = TextStyle(
+        fontFamily = hannar,
         fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
+        fontSize = 10.sp,
+        letterSpacing = 1.5.sp
     )
-    */
 )
