c785334742f8d5419e27b4e5858361dcf7c68cac
super7615 <super7615@naver.com>
2022-10-31 16:21:16 +0900

    feat : MainActivity (S07P31D203-156)

--- OurHome/presentation/src/main/java/com/ssafy/ourhome/MainActivity.kt
+++ OurHome/presentation/src/main/java/com/ssafy/ourhome/MainActivity.kt
@@ -3,13 +3,17 @@ package com.ssafy.ourhome
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.ssafy.ourhome.components.EditTextPreView
+import com.ssafy.ourhome.components.RoundedButton
 import com.ssafy.ourhome.ui.theme.OurHomeTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +21,29 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             OurHomeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+//                MyApp()
+                EditTextPreView()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MyApp() {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colors.background
+    ) {
+        Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
+            RoundedButton(label = "우리집")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     OurHomeTheme {
-        Greeting("Android")
+        MyApp()
     }
 }
