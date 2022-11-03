36a8b0af39d4ce56988f95cbc2c43da3a5b93863
DESKTOP-KVCQHCD\multicampus <dkeka1187@naver.com>
2022-11-03 17:59:20 +0900

    feat : 키보드 포커스 추적 기능 추가

--- OurHome/presentation/src/main/java/com/ssafy/ourhome/utils/Extensions.kt
+++ OurHome/presentation/src/main/java/com/ssafy/ourhome/utils/Extensions.kt
@@ -3,6 +3,11 @@ package com.ssafy.ourhome.utils
 import android.app.Activity
 import android.content.Context
 import android.content.ContextWrapper
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusManager
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalFocusManager
 
 fun Context.findActivity(): Activity {
     var context = this
@@ -11,4 +16,14 @@ fun Context.findActivity(): Activity {
         context = context.baseContext
     }
     throw IllegalStateException("no activity")
+}
+
+/** 에딧텍스트 포커스 관리 함수 (키보드 닫힘사용) **/
+fun Modifier.addFocusCleaner(focusManager: FocusManager, doOnClear: () -> Unit = {}): Modifier {
+    return this.pointerInput(Unit) {
+        detectTapGestures(onTap = {
+            doOnClear()
+            focusManager.clearFocus()
+        })
+    }
 }
