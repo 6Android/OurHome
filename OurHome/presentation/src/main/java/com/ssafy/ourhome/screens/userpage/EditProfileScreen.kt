2fd323b8d3cd7295e3007eb2fd209c19361958bd
DESKTOP-KVCQHCD\multicampus <jdsaeyqi@naver.com>
2022-11-04 15:59:11 +0900

    feat : 정보 수정 페이지 구현 (S07P31D203-197)

--- OurHome/presentation/src/main/java/com/ssafy/ourhome/screens/userpage/EditProfileScreen.kt
+++ OurHome/presentation/src/main/java/com/ssafy/ourhome/screens/userpage/EditProfileScreen.kt
@@ -1,26 +1,23 @@
 package com.ssafy.ourhome.screens.userpage
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material.icons.filled.Clear
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
@@ -28,13 +25,14 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.PopupProperties
 import androidx.navigation.NavController
 import coil.compose.rememberAsyncImagePainter
 import com.ssafy.ourhome.R
 import com.ssafy.ourhome.components.MainAppBar
-import com.ssafy.ourhome.components.NoLabelTextInput
 import com.ssafy.ourhome.components.OurHomeSurface
 import com.ssafy.ourhome.navigation.OurHomeScreens
+import com.ssafy.ourhome.ui.theme.MainColor
 import com.ssafy.ourhome.ui.theme.OurHomeTheme
 
 data class tmpUserDTO(
@@ -66,8 +64,25 @@ fun EditProfileScreen(navController: NavController = NavController(LocalContext.
     val phoneState = remember {
         mutableStateOf(user.phone)
     }
+    val bloodTypeState = remember {
+        mutableStateOf(user.bloodType)
+    }
 
+    val MBTIState = remember {
+        mutableStateOf(user.MBTI)
+    }
 
+    val jobState = remember {
+        mutableStateOf(user.job)
+    }
+
+    val interestState = remember {
+        mutableStateOf(user.interest)
+    }
+
+    val hobbyState = remember {
+        mutableStateOf(user.hobby)
+    }
 
     OurHomeSurface() {
         Scaffold(topBar = {
@@ -86,7 +101,7 @@ fun EditProfileScreen(navController: NavController = NavController(LocalContext.
                     .fillMaxSize()
                     .verticalScroll(scrollState),
                 horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.spacedBy(20.dp)
+                verticalArrangement = Arrangement.spacedBy(24.dp)
             ) {
                 Spacer(modifier = Modifier.height(16.dp))
 
@@ -100,7 +115,9 @@ fun EditProfileScreen(navController: NavController = NavController(LocalContext.
                     TextFieldWithClear(nicknameState)
                 }
 
-                Divider(thickness = 8.dp, color = Color.LightGray.copy(alpha = 0.8f))
+                Divider(
+                    modifier = Modifier.padding(bottom = 24.dp),
+                    thickness = 4.dp, color = Color.LightGray.copy(alpha = 0.8f))
 
                 Column(
                     modifier = Modifier
@@ -110,6 +127,30 @@ fun EditProfileScreen(navController: NavController = NavController(LocalContext.
                 ) {
                     TextWithText("이메일", user.email)
                     TextWithTextField("전화번호", phoneState)
+                    TextWithText(title = "생일", content = user.birth)
+                    TextWithDropDown(
+                        title = "혈액형", textState = bloodTypeState, itemList =
+                        listOf(
+                            "Rh+ A",
+                            "Rh- A",
+                            "Rh+ B",
+                            "Rh- B",
+                            "Rh+ O",
+                            "Rh- O",
+                            "Rh+ AB",
+                            "Rh- AB"
+                        )
+                    )
+                    TextWithDropDown(
+                        title = "MBTI", textState = MBTIState, itemList =
+                        listOf(
+                            "ENFP","ENFJ","ENTP","ENTJ","ESFP","ESFJ","ESTP","ESTJ",
+                            "INFP","INFJ","INTP","INTJ","ISFP","ISFJ","ISTP","ISTJ",
+                        )
+                    )
+                    TextWithTextField("직업", jobState)
+                    TextWithTextField("관심사", interestState)
+                    TextWithTextField("취미", hobbyState)
                 }
                 Spacer(modifier = Modifier.height(16.dp))
             }
@@ -117,11 +158,112 @@ fun EditProfileScreen(navController: NavController = NavController(LocalContext.
     }
 }
 
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+private fun TextWithDropDown(
+    title: String,
+    textState: MutableState<String>,
+    itemList: List<String>
+) {
+
+    var expanded by remember {
+        mutableStateOf(false)
+    }
+
+    var selectedIndex by remember {
+
+        mutableStateOf(itemList.indexOf(textState.value))
+    }
+
+    Row(modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
+        Text(
+            text = title, modifier = Modifier.weight(2f),
+            style = MaterialTheme.typography.h6.copy(fontWeight = FontWeight.Bold)
+        )
+        Box(modifier = Modifier
+            .fillMaxHeight()
+            .weight(4f)) {
+
+//            ExposedDropdownMenuBox(
+//                expanded = expanded,
+//                onExpandedChange = {
+//                    expanded = !expanded
+//                },
+//                modifier = Modifier.wrapContentHeight()
+//            ) {
+//                TextField(
+//                    readOnly = true,
+//                    value = itemList[selectedIndex],
+//                    onValueChange = { },
+//                    trailingIcon = {
+//                        ExposedDropdownMenuDefaults.TrailingIcon(
+//                            expanded = expanded
+//                        )
+//                    },
+//                    colors = ExposedDropdownMenuDefaults.textFieldColors()
+//                )
+//                ExposedDropdownMenu(
+//                    expanded = expanded,
+//                    onDismissRequest = {
+//                        expanded = false
+//                    }
+//                ) {
+//                    itemList.forEachIndexed { idx, selectionOption ->
+//                        DropdownMenuItem(
+//                            onClick = {
+//                               selectedIndex = idx
+//                                expanded = false
+//                            }
+//                        ) {
+//                            Text(text = selectionOption, style = MaterialTheme.typography.h6)
+//                        }
+//                    }
+//                }
+//            }
+            Row(
+                Modifier
+                    .fillMaxWidth()
+                    .height(30.dp), verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.SpaceBetween) {
+                Text(
+                    text = itemList[selectedIndex], modifier = Modifier,
+                    style = MaterialTheme.typography.h6.copy(fontWeight = FontWeight.Bold)
+                )
+
+                IconButton(
+                    onClick = { expanded = true }) {
+                    Icon(imageVector = Icons.Default.ArrowDropDown, contentDescription = "Drop Down Button",
+                        modifier = Modifier.size(20.dp))
+                }
+            }
+
+
+            DropdownMenu(
+                expanded = expanded, onDismissRequest = { expanded = false },
+                modifier = Modifier
+                    .wrapContentHeight()
+                    .heightIn(max = 235.dp)
+
+            ) {
+                itemList.forEachIndexed { index, item ->
+                    DropdownMenuItem(modifier = Modifier.wrapContentHeight(),onClick = {
+                        selectedIndex = index
+                        expanded = false
+                    }) {
+                        Text(text = item,style = MaterialTheme.typography.h6)
+                    }
+                }
+            }
+        }
+
+
+    }
+}
+
 @Composable
 private fun TextFieldWithClear(
     textState: MutableState<String>
 ) {
-    Box(modifier = Modifier.width(240.dp),) {
+    Box(modifier = Modifier.width(240.dp)) {
 
         NoLabelTextInput(
             modifier = Modifier.align(Alignment.Center),
@@ -133,9 +275,13 @@ private fun TextFieldWithClear(
 
         Icon(imageVector = Icons.Default.Clear,
             contentDescription = "Clear Button",
-            modifier = Modifier.align(Alignment.CenterEnd).clickable {
-                textState.value = ""
-            }
+            modifier = Modifier
+
+                .align(Alignment.CenterEnd)
+                .clickable {
+                    textState.value = ""
+                }
+                .size(20.dp)
         )
     }
 
@@ -147,8 +293,10 @@ private fun TextWithText(
     content: String
 ) {
     Row(modifier = Modifier.fillMaxWidth()) {
-        Text(text = title, modifier = Modifier.weight(2f),
-            style = MaterialTheme.typography.h6.copy(fontWeight = FontWeight.Bold))
+        Text(
+            text = title, modifier = Modifier.weight(2f),
+            style = MaterialTheme.typography.h6.copy(fontWeight = FontWeight.Bold)
+        )
         Text(text = content, modifier = Modifier.weight(4f), style = MaterialTheme.typography.h6)
     }
 }
@@ -159,8 +307,10 @@ private fun TextWithTextField(
     textState: MutableState<String>
 ) {
     Row(modifier = Modifier.fillMaxWidth()) {
-        Text(text = title, modifier = Modifier.weight(2f),
-            style = MaterialTheme.typography.h6.copy(fontWeight = FontWeight.Bold))
+        Text(
+            text = title, modifier = Modifier.weight(2f),
+            style = MaterialTheme.typography.h6.copy(fontWeight = FontWeight.Bold)
+        )
         NoLabelTextInput(
             modifier = Modifier.weight(4f),
             valueState = textState,
@@ -187,6 +337,7 @@ private fun UserImage(
 }
 
 /** 라벨이 없고, 길이 제한이 있는 한줄 editText **/
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 private fun NoLabelTextInput(
     modifier: Modifier = Modifier,
@@ -199,6 +350,7 @@ private fun NoLabelTextInput(
     imeAction: ImeAction = ImeAction.Done,
     onAction: KeyboardActions = KeyboardActions.Default
 ) {
+    val keyboardController = LocalSoftwareKeyboardController.current
     BasicTextField(
         value = valueState.value,
         onValueChange = {
@@ -215,7 +367,21 @@ private fun NoLabelTextInput(
         modifier = modifier.fillMaxWidth(),
         enabled = enabled,
         keyboardOptions = KeyboardOptions(keyboardType = keyboardType, imeAction = imeAction),
-        keyboardActions = onAction,
+        keyboardActions = KeyboardActions(onDone = {
+            keyboardController?.hide()
+        }),
+        decorationBox = { innerTextField ->
+
+            Box(
+                modifier = Modifier
+                    .height(25.dp),
+                contentAlignment = Alignment.BottomCenter
+            ) {
+                Divider(Modifier.height(1.dp), color = Color.LightGray)
+            }
+            innerTextField()
+
+        }
     )
 }
 
