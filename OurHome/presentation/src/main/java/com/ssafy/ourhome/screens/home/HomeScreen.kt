a1562e6acba446847d626272d2b860b76f640491
vkdnjxodnd <vkdnjxodnd@naver.com>
2022-11-04 13:20:12 +0900

    refact : 캘린더 컴포넌트 분리 (S07P31D203-183)

--- OurHome/presentation/src/main/java/com/ssafy/ourhome/screens/home/HomeScreen.kt
+++ OurHome/presentation/src/main/java/com/ssafy/ourhome/screens/home/HomeScreen.kt
@@ -1,17 +1,17 @@
 package com.ssafy.ourhome.screens.home
 
-import android.annotation.SuppressLint
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.*
-import androidx.compose.runtime.*
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -20,9 +20,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -32,22 +30,11 @@ import androidx.navigation.NavController
 import coil.compose.rememberAsyncImagePainter
 import com.holix.android.bottomsheetdialog.compose.BottomSheetDialog
 import com.holix.android.bottomsheetdialog.compose.BottomSheetDialogProperties
-import com.kizitonwose.calendar.compose.CalendarState
-import com.kizitonwose.calendar.compose.HorizontalCalendar
-import com.kizitonwose.calendar.compose.rememberCalendarState
-import com.kizitonwose.calendar.core.*
+import com.kizitonwose.calendar.core.CalendarDay
 import com.ssafy.ourhome.R
 import com.ssafy.ourhome.components.OurHomeSurface
-import com.ssafy.ourhome.ui.theme.*
+import com.ssafy.ourhome.ui.theme.OurHomeTheme
 import com.ssafy.ourhome.utils.Schedule
-import com.ssafy.ourhome.utils.getSchedules
-import kotlinx.coroutines.flow.filter
-import kotlinx.coroutines.launch
-import java.time.DayOfWeek
-import java.time.Month
-import java.time.YearMonth
-import java.time.format.TextStyle
-import java.util.*
 
 
 data class Person(
@@ -414,303 +401,6 @@ private fun NoScheduleItem() {
         style = MaterialTheme.typography.body1.copy(fontWeight = FontWeight.Bold),
     )
     Spacer(modifier = Modifier.height(16.dp))
-}
-
-/** 오늘 일정 리스트  **/
-@Composable
-fun TodayScheduleList(modifier: Modifier = Modifier, list: List<Schedule>) {
-    LazyColumn(modifier = modifier) {
-        items(list) { item ->
-            TodayScheduleListItem(schedule = item)
-        }
-    }
-}
-
-/** 오늘 일정 리스트 아이템 **/
-@Composable
-fun TodayScheduleListItem(modifier: Modifier = Modifier.fillMaxWidth(), schedule: Schedule) {
-    Card(
-        modifier = modifier
-            .padding(vertical = 8.dp)
-            .shadow(elevation = 2.dp, shape = RoundedCornerShape(8.dp))
-    ) {
-        Column(
-            modifier = modifier.clickable {
-                // todo: 일정 상세로 이동
-            }
-        ) {
-            Row(
-                modifier = modifier
-                    .padding(12.dp)
-            ) {
-                Image(
-                    modifier = Modifier.size(12.dp),
-                    painter = painterResource(id = R.drawable.img_calendar_circle),
-                    contentDescription = ""
-                )
-
-                Text(
-                    modifier = Modifier.padding(start = 8.dp),
-                    text = schedule.date,
-                    fontFamily = nanum,
-                    color = Gray,
-                    fontWeight = FontWeight.Normal,
-                    fontSize = 12.sp
-                )
-            }
-
-            Text(
-                modifier = modifier
-                    .padding(start = 12.dp, bottom = 16.dp),
-                text = schedule.title,
-                fontFamily = nanum,
-                fontWeight = FontWeight.Bold,
-                fontSize = 16.sp
-            )
-        }
-    }
-
-}
-
-/** 달력 카드 */
-@Composable
-fun CalendarCard(
-    modifier: Modifier = Modifier,
-    visibleBottomSheetState: MutableState<Boolean>,
-    selection: MutableState<CalendarDay?>,
-    map: MutableMap<String, List<Schedule>>
-) {
-    Card(
-        modifier = modifier
-            .fillMaxWidth()
-            .height(430.dp)
-            .shadow(elevation = 2.dp, shape = RoundedCornerShape(16.dp))
-    ) {
-        Column(
-            modifier = Modifier
-                .padding(start = 16.dp, end = 16.dp, bottom = 16.dp)
-        ) {
-            Spacer(modifier = Modifier.height(16.dp))
-            /** 달력 */
-            Calendar(
-                visibleBottomSheetState = visibleBottomSheetState,
-                selection = selection,
-                map = map
-            )
-
-            /**
-            todo
-            월화수목 금토일 넣기
-            오늘 날짜 표시하기 (2022년 10월)
-            < > 버튼 누르면 달력 이동
-            날짜 누르면 바텀sheet으로 해당하는 날짜의 일정 뜸
-            이번달 일 색만 검은색 나머지는 회색
-             */
-        }
-    }
-}
-
-/** 달력 */
-@SuppressLint("UnrememberedMutableState")
-@Composable
-fun Calendar(
-    visibleBottomSheetState: MutableState<Boolean>,
-    selection: MutableState<CalendarDay?>,
-    map: MutableMap<String, List<Schedule>>
-) {
-    val currentMonth = remember { YearMonth.now() }
-    val startMonth = remember { currentMonth.minusMonths(500) } // Adjust as needed
-    val endMonth = remember { currentMonth.plusMonths(500) } // Adjust as needed
-    val daysOfWeek = remember { daysOfWeek() }
-
-    val state = rememberCalendarState(
-        startMonth = startMonth,
-        endMonth = endMonth,
-        firstVisibleMonth = currentMonth,
-        firstDayOfWeek = daysOfWeek.first()
-    )
-    val coroutineScope = rememberCoroutineScope()
-    val visibleMonth = rememberFirstVisibleMonthAfterScroll(state)
-
-    // 달력 스크롤 시 이전에 선택한 날짜 초기화
-//    LaunchedEffect(visibleMonth) {
-//        selection = null
-//    }
-
-    visibleMonth.let {
-        map.putAll(getSchedules(it).groupBy { schedule -> schedule.date })
-    }
-
-    CalendarTitle(
-        modifier = Modifier.padding(vertical = 10.dp, horizontal = 8.dp),
-        currentMonth = visibleMonth,
-        goToPrevious = {
-            coroutineScope.launch {
-                state.animateScrollToMonth(state.firstVisibleMonth.yearMonth.previousMonth)
-            }
-        },
-        goToNext = {
-            coroutineScope.launch {
-                state.animateScrollToMonth(state.firstVisibleMonth.yearMonth.nextMonth)
-            }
-        }
-    )
-    Spacer(modifier = Modifier.height(24.dp))
-    HorizontalCalendar(
-        state = state,
-        dayContent = { day ->
-            Day(
-                day = day,
-                isSelected = selection.value == day,
-                hasSchedule = map.containsKey("${day.date.year}-${day.date.monthValue}-${day.date.dayOfMonth}"),
-            ) { clickedDay ->
-                selection.value = clickedDay
-                visibleBottomSheetState.value = true
-            }
-        },
-        monthHeader = {
-            DaysOfWeekTitle(daysOfWeek = daysOfWeek)
-        }
-    )
-}
-
-/** 현재 년, 월, 이전, 다음 버튼 */
-@Composable
-fun CalendarTitle(
-    modifier: Modifier,
-    currentMonth: YearMonth,
-    goToPrevious: () -> Unit,
-    goToNext: () -> Unit,
-) {
-    Row(
-        modifier = modifier,
-        verticalAlignment = Alignment.CenterVertically,
-    ) {
-        Box(
-            modifier = Modifier
-                .size(40.dp)
-                .clip(shape = CircleShape)
-                .clickable(role = Role.Button, onClick = goToPrevious)
-        ) {
-            Icon(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(4.dp)
-                    .align(Alignment.Center),
-                imageVector = Icons.Default.ChevronLeft,
-                contentDescription = "Previous",
-                tint = Color.Gray
-            )
-        }
-        Text(
-            modifier = Modifier
-                .weight(1f)
-                .testTag("MonthTitle"),
-            text = currentMonth.displayText(),
-            style = MaterialTheme.typography.subtitle2,
-            textAlign = TextAlign.Center,
-        )
-        Box(
-            modifier = Modifier
-                .size(40.dp)
-                .clip(shape = CircleShape)
-                .clickable(role = Role.Button, onClick = goToNext)
-        ) {
-            Icon(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(4.dp)
-                    .align(Alignment.Center),
-                imageVector = Icons.Default.ChevronRight,
-                contentDescription = "Next",
-                tint = Color.Gray
-            )
-        }
-    }
-}
-
-/** 요일 타이틀 표시 */
-@Composable
-fun DaysOfWeekTitle(daysOfWeek: List<DayOfWeek>) {
-    Row(modifier = Modifier.fillMaxWidth()) {
-        for (dayOfWeek in daysOfWeek) {
-            Text(
-                modifier = Modifier.weight(1f),
-                textAlign = TextAlign.Center,
-                text = dayOfWeek.getDisplayName(TextStyle.SHORT, Locale.getDefault()),
-                fontWeight = FontWeight.Bold
-            )
-        }
-    }
-}
-
-/** 일 */
-@Composable
-fun Day(
-    day: CalendarDay,
-    isSelected: Boolean,
-    hasSchedule: Boolean,
-    onClick: (CalendarDay) -> Unit
-) {
-    Box(
-        modifier = Modifier
-            .aspectRatio(1f)
-            .clip(CircleShape)
-            .background(color = if (isSelected) MainColor else Color.Transparent)
-            .clickable(
-                enabled = day.position == DayPosition.MonthDate,
-                onClick = { onClick(day) }
-            ),
-        contentAlignment = Alignment.Center
-    ) {
-        val textColor = when (day.position) {
-            DayPosition.MonthDate ->
-                if (isSelected) {
-                    Color.White
-                } else {
-                    Color.Black
-                }
-            else -> Color.LightGray
-        }
-        Text(
-            text = day.date.dayOfMonth.toString(),
-            style = MaterialTheme.typography.body2,
-            color = textColor
-        )
-        if (hasSchedule) {
-            Box(
-                modifier = Modifier
-                    .size(5.dp)
-                    .clip(shape = CircleShape)
-                    .background(Green)
-                    .align(alignment = Alignment.BottomCenter)
-            ) { }
-        }
-    }
-}
-
-
-/**
-Extensions
- */
-@Composable
-fun rememberFirstVisibleMonthAfterScroll(state: CalendarState): YearMonth {
-    val visibleMonth = remember(state) { mutableStateOf(state.firstVisibleMonth.yearMonth) }
-    LaunchedEffect(state) {
-        snapshotFlow { state.isScrollInProgress }
-            .filter { scrolling -> !scrolling }
-            .collect { visibleMonth.value = state.firstVisibleMonth.yearMonth }
-    }
-    return visibleMonth.value
-}
-
-fun YearMonth.displayText(short: Boolean = false): String {
-    return "${this.year}년 ${this.month.displayText(short = short)} 가족일정"
-}
-
-fun Month.displayText(short: Boolean = true): String {
-    val style = if (short) TextStyle.SHORT else TextStyle.FULL
-    return getDisplayName(style, Locale.KOREA)
 }
 
 /**
--- OurHome/presentation/src/main/java/com/ssafy/ourhome/utils/Extensions.kt
+++ OurHome/presentation/src/main/java/com/ssafy/ourhome/utils/Extensions.kt
@@ -3,6 +3,13 @@ package com.ssafy.ourhome.utils
 import android.app.Activity
 import android.content.Context
 import android.content.ContextWrapper
+import androidx.compose.runtime.*
+import com.kizitonwose.calendar.compose.CalendarState
+import kotlinx.coroutines.flow.filter
+import java.time.Month
+import java.time.YearMonth
+import java.time.format.TextStyle
+import java.util.*
 
 fun Context.findActivity(): Activity {
     var context = this
@@ -11,4 +18,25 @@ fun Context.findActivity(): Activity {
         context = context.baseContext
     }
     throw IllegalStateException("no activity")
+}
+
+/** 캘린더 */
+@Composable
+fun rememberFirstVisibleMonthAfterScroll(state: CalendarState): YearMonth {
+    val visibleMonth = remember(state) { mutableStateOf(state.firstVisibleMonth.yearMonth) }
+    LaunchedEffect(state) {
+        snapshotFlow { state.isScrollInProgress }
+            .filter { scrolling -> !scrolling }
+            .collect { visibleMonth.value = state.firstVisibleMonth.yearMonth }
+    }
+    return visibleMonth.value
+}
+
+fun YearMonth.displayText(short: Boolean = false): String {
+    return "${this.year}년 ${this.month.displayText(short = short)} 가족일정"
+}
+
+fun Month.displayText(short: Boolean = true): String {
+    val style = if (short) TextStyle.SHORT else TextStyle.FULL
+    return getDisplayName(style, Locale.KOREA)
 }
