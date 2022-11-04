29a2fbad4cccabac678bbcd6549769af1d009c28
DESKTOP-KVCQHCD\multicampus <dkeka1187@naver.com>
2022-11-04 17:58:18 +0900

    feat : 앨범 상세 화면 UI 구현 중

--- OurHome/presentation/src/main/java/com/ssafy/ourhome/screens/album/AlbumDetailScreen.kt
+++ OurHome/presentation/src/main/java/com/ssafy/ourhome/screens/album/AlbumDetailScreen.kt
@@ -1,32 +1,82 @@
 package com.ssafy.ourhome.screens.album
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import coil.compose.AsyncImagePainter.State.Empty.painter
 import coil.compose.rememberAsyncImagePainter
+import com.google.accompanist.systemuicontroller.SystemUiController
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 @Composable
 fun AlbumDetailScreen(navController : NavController, photoUrl: String, photoDate: String){
-    Column(modifier = Modifier.fillMaxSize()) {
-        Text(text = "AlbumDetailScreen")
-        Text(text = "$photoUrl")
+    val systemUiController: SystemUiController = rememberSystemUiController()
+    systemUiController.isStatusBarVisible = false // Status bar
 
-        val painter =
-            rememberAsyncImagePainter(photoUrl)
+    val painter = rememberAsyncImagePainter(photoUrl)
 
-        Image(
-            modifier = Modifier.size(250.dp),
-            painter = painter,
-            contentDescription = "펫 이미지"
-        )
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ){
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(color = Color.Black),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                modifier = Modifier.fillMaxSize(),
+                painter = painter,
+                contentDescription = "펫 이미지",
+                contentScale = ContentScale.Fit
+            )
+        }
 
-        Text(text = "$photoDate")
+
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(all = 16.dp)
+                .background(color = Color(0x55000000)),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Icon(
+                imageVector = Icons.Default.ArrowBack,
+                contentDescription = "BackButton",
+                modifier = Modifier.clickable {
+                    navController.popBackStack()
+                },
+                tint = Color.White
+            )
+            
+            Spacer(modifier = Modifier.width(12.dp))
+
+            Text(
+                text = "$photoDate",
+                style = MaterialTheme.typography.h6.copy(fontWeight = FontWeight.Bold),
+                color = Color.White,
+            )
+        }
+
+
+
     }
 
 }
