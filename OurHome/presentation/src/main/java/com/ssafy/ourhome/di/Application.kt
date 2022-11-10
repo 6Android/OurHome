cb271f30d286e551a2b22b0f9cb321b169409afd
SangyeopHan <super7615@naver.com>
2022-11-10 09:49:37 +0900

    feat : 워크매니저 초기화 (S07P31D203-233)

--- OurHome/presentation/src/main/java/com/ssafy/ourhome/di/Application.kt
+++ OurHome/presentation/src/main/java/com/ssafy/ourhome/di/Application.kt
@@ -1,9 +1,19 @@
 package com.ssafy.ourhome.di
 
 import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class ApplicationClass: Application() {
+class ApplicationClass : Application(), Configuration.Provider {
 
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
+
+    override fun getWorkManagerConfiguration(): Configuration =
+        Configuration.Builder()
+            .setWorkerFactory(workerFactory)
+            .build()
 }
